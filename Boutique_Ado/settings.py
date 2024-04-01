3263f50059945a5342f8b1ae107e218c8cf42b92
VitorBarbosa1 <VITORBARBOSA1301@GMAIL.COM>
2024-04-02 00:58:27 +0100

    059 - Deployement part 5

--- Boutique_Ado/settings.py
+++ Boutique_Ado/settings.py
@@ -32,7 +32,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['https://project---boutique-ado-c0f4f70f43b7.herokuapp.com/', 'localhost',]
+ALLOWED_HOSTS = ['project---boutique-ado-c0f4f70f43b7.herokuapp.com', 'localhost', '127.0.0.1']
+
+
 
 
 # Application definition
@@ -56,6 +58,7 @@ INSTALLED_APPS = [
 
     # Other apps
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -184,6 +187,28 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
+if 'USE_AWS' in os.environ:
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'project---boutique-ado'
+    AWS_S3_REGION_NAME = 'eu-north-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
+
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
