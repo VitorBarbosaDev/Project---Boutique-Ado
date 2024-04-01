e0b130c7fa9559aa0c9282e2c14752add10f0d94
Vitor Barbosa <VITORBARBOSA1301@GMAIL.COM>
2024-04-01 15:05:07 +0100

    Product admin - Finsihed product add function

--- profiles/views.py
+++ profiles/views.py
@@ -1,46 +1,91 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.db.models import Q
+from django.db.models.functions import Lower
 
-from .models import UserProfile
-from .forms import UserProfileForm
+from .models import Product, Category
+from .forms import ProductForm
 
-from checkout.models import Order
+# Create your views here.
 
-def profile(request):
-    """ Display the user's profile. """
-    profile = get_object_or_404(UserProfile, user=request.user)
+def all_products(request):
+    """ A view to show all products, including sorting and search queries """
 
-    if request.method == 'POST':
-        form = UserProfileForm(request.POST, instance=profile)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Profile updated successfully')
+    products = Product.objects.all()
+    query = None
+    categories = None
+    sort = None
+    direction = None
 
-    form = UserProfileForm(instance=profile)
-    orders = profile.orders.all()
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
 
-    template = 'profiles/profile.html'
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
+
     context = {
-        'form': form,
-        'orders': orders,
-        'on_profile_page': True
+        'products': products,
+        'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
-    return render(request, template, context)
+    return render(request, 'products/products.html', context)
 
 
-def order_history(request, order_number):
-    order = get_object_or_404(Order, order_number=order_number)
+def product_detail(request, product_id):
+    """ A view to show individual product details """
 
-    messages.info(request, (
-        f'This is a past confirmation for order number {order_number}. '
-        'A confirmation email was sent on the order date.'
-    ))
+    product = get_object_or_404(Product, pk=product_id)
 
-    template = 'checkout/checkout_success.html'
     context = {
-        'order': order,
-        'from_profile': True,
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
     }
 
     return render(request, template, context)
