00254213c48b4af214197970d9028dda19fd9e12
VitorBarbosa1 <VITORBARBOSA1301@GMAIL.COM>
2024-04-02 20:38:11 +0100

    add email varaiables

--- Boutique_Ado/settings.py
+++ Boutique_Ado/settings.py
@@ -222,8 +222,20 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
-STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '') # Webhook secret Can't get this yet
-DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+
+# Webhook secret Can't get this yet
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
